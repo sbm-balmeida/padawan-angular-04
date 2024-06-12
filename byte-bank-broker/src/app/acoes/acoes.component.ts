0928885d2e805e63f874fd7b3a2880a74034a3ce
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-12 15:51:48 -0300

    DebounceTime

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -2,7 +2,9 @@ import { AcoesService } from './acoes.service';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { merge } from 'rxjs';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -19,6 +21,7 @@ export class AcoesComponent {
   );
 
   filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
     tap(() => {console.log('Fluxo do filtro')}),
     tap(console.log),
     filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
