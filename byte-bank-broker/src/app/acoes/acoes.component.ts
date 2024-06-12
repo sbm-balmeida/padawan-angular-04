ef0ac1124bed067119f47f983dd5183f1d71d2f6
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-12 14:54:14 -0300

    Eventos do Input

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,8 +1,7 @@
-import { Subscription } from 'rxjs';
 import { AcoesService } from './acoes.service';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Acoes } from './modelo/acoes';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-acoes',
@@ -11,7 +10,7 @@ import { Acoes } from './modelo/acoes';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesService.getAcoes();
+  acoes$ = this.acoesInput.valueChanges.pipe(tap(console.log))
 
   constructor(private acoesService: AcoesService) {}
 }
