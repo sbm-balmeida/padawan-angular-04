1804f7c3254c109da7431e062d9a774062e03f9c
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-12 14:04:15 -0300

    Unsubscribe

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,5 +1,6 @@
+import { Subscription } from 'rxjs';
 import { AcoesService } from './acoes.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Acoes } from './modelo/acoes';
 
@@ -8,16 +9,22 @@ import { Acoes } from './modelo/acoes';
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit {
+export class AcoesComponent implements OnInit, OnDestroy {
   acoesInput = new FormControl();
 
   acoes: Acoes;
 
+  private subscription: Subscription;
+
   constructor(private acoesService: AcoesService) {}
 
-  ngOnInit(): void {
-    this.acoesService.getAcoes().subscribe((acoes) => {
+  ngOnInit() {
+    this.subscription = this.acoesService.getAcoes().subscribe((acoes) => {
       this.acoes = acoes;
     })
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
